f0280b2da37b2231b708d44234c886aae343d069
Jeong Doosik <entlrsla@gmail.com>
2025-06-25 07:09:23 +0900

    user entity생성

---
+++ src/main/java/com/doosik/jwtproject/domain/User.java
@@ -0,0 +1,33 @@
+package com.doosik.jwtproject.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long Id;
+	
+	@Column(nullable = false, unique = true)
+	private String username;
+
+	@Column(nullable = false)
+	private String password;
+	
+	private String role; //user role
+}
